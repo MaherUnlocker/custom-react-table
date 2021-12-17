c28d936a2d5f22748491a85ea065aa6c60e36654
M@HeR <maher.benbaccar@aureskonnect.com>
2021-12-17 17:12:24 +0100

    fix: error in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,105 @@
-import { CssBaseline, MenuItem, TextField } from "@material-ui/core";
-import axios from "axios";
-import React, { useCallback } from "react";
-import {
-  CellProps,
-  FilterProps,
-  FilterValue,
-  IdType,
-  Row,
-  TableInstance
-} from "react-table";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { Table } from "./Table";
-import { PersonData, makeData } from "./utils";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { CssBaseline, MenuItem, TextField } from '@material-ui/core'
+import axios from 'axios'
+import React from 'react'
+import { CellProps, FilterProps, FilterValue, IdType, Row, TableInstance } from 'react-table'
+
+import { Table } from './Table'
+import { PersonData, makeData } from './utils'
 
 // This is a custom aggregator that
 // takes in an array of values and
 // returns the rounded median
 
-function filterGreaterThan(
-  rows: Array<Row<any>>,
-  id: Array<IdType<any>>,
-  filterValue: FilterValue
-) {
+function filterGreaterThan(rows: Array<Row<any>>, id: Array<IdType<any>>, filterValue: FilterValue) {
   return rows.filter((row) => {
-    const rowValue = row.values[id[0]];
-    return rowValue >= filterValue;
-  });
+    const rowValue = row.values[id[0]]
+    return rowValue >= filterValue
+  })
 }
 
 // This is an autoRemove method on the filter function that
 // when given the new filter value and returns true, the filter
 // will be automatically removed. Normally this is just an undefined
 // check, but here, we want to remove the filter if it's not a number
-filterGreaterThan.autoRemove = (val: any) => typeof val !== "number";
+filterGreaterThan.autoRemove = (val: any) => typeof val !== 'number'
 
 function SelectColumnFilter({
-  column: { filterValue, render, setFilter, preFilteredRows, id }
+  column: { filterValue, render, setFilter, preFilteredRows, id },
 }: FilterProps<PersonData>) {
   const options = React.useMemo(() => {
-    const options = new Set<any>();
+    const options = new Set<any>()
     preFilteredRows.forEach((row) => {
-      options.add(row.values[id]);
-    });
-    return [...Array.from(options.values())];
-  }, [id, preFilteredRows]);
+      options.add(row.values[id])
+    })
+    return [...Array.from(options.values())]
+  }, [id, preFilteredRows])
 
   return (
     <TextField
       select
-      label={render("Header")}
-      value={filterValue || ""}
+      label={render('Header')}
+      value={filterValue || ''}
       onChange={(e) => {
-        setFilter(e.target.value || undefined);
+        setFilter(e.target.value || undefined)
       }}
     >
-      <li value={""}>All</li>
+      <li value={''}>All</li>
       {options.map((option, i) => (
         <MenuItem key={i} value={option}>
           {option}
         </MenuItem>
       ))}
     </TextField>
-  );
+  )
 }
 
 const App: React.FC = () => {
-  const [apiResult, setApiResult] = React.useState<any[]>([]);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<null | any>(null);
+  const [apiResult, setApiResult] = React.useState<any[]>([])
+  const [loading, setLoading] = React.useState<boolean>(false)
+  const [error, setError] = React.useState<null | any>(null)
   async function fetchData(url: any) {
     await axios
       .get(url)
       .then((response) => {
-        setApiResult(response.data);
+        setApiResult(response.data)
       })
       .catch((err) => {
-        setError(err);
+        setError(err)
       })
       .finally(() => {
-        setLoading(false);
-      });
+        setLoading(false)
+      })
   }
   // const [apiResult] = React.useState<PersonData[]>(() => makeData(10000));
   const apiResultColumns = React.useMemo(
     () =>
       apiResult[0]
         ? Object.keys(apiResult[0])
-            .filter((key) => key !== "rating")
+            .filter((key) => key !== 'rating')
             .map((key) => {
-              if (key === "image") {
+              if (key === 'image') {
                 return {
                   Header: key,
                   accessor: key,
                   disableFilters: true,
                   Cell: (value: any) => {
-                    return (
-                      <img
-                        src={value.cell.value}
-                        className="h-25 w-25"
-                        alt=""
-                      />
-                    );
-                  }
-                };
+                    return <img src={value.cell.value} className='h-25 w-25' alt='' />
+                  },
+                }
               }
 
               return {
                 Header: key,
                 accessor: key,
-                aggregate: "count",
-                Aggregated: ({ cell: { value } }: CellProps) => `${value} Names`
-              };
+                aggregate: 'count',
+                Aggregated: ({ cell: { value } }: any) => `${value} Names`,
+              }
             })
         : [],
     [apiResult]
-  );
+  )
 
   // const columns: any = React.useMemo(() => apiResultColumns, [
   //   apiResultColumns
@@ -124,17 +108,17 @@ const App: React.FC = () => {
   // const [apiResult] = React.useState<PersonData[]>(() => makeData(10000));
   // const [data] = React.useMemo(() => [...apiResult], [apiResult]);
   React.useEffect(() => {
-    fetchData("https://jsonplaceholder.typicode.com/todos");
-  }, []);
+    fetchData('https://jsonplaceholder.typicode.com/todos')
+  }, [])
 
   // console.log(columns);
   // console.log(data);
   return (
     <div>
       <CssBaseline />
-      <Table name={"testTable"} columns={apiResultColumns} data={apiResult} />
+      <Table name={'testTable'} columns={apiResultColumns} data={apiResult} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

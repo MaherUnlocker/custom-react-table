8979daee704d0fba6df4b22439cc8701a64baee6
M@HeR <maher.benbaccar@aureskonnect.com>
2021-12-23 14:51:39 +0100

    fix: core app compoent

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,31 @@ import React from 'react'
 import Dynamictable from '../src/Table/Dynamictable'
 
 export default function App() {
+  function ActionColumn() {
+    return (
+      <React.Fragment>
+        <button
+          className='btn btn-secondary dropdown-toggle'
+          type='button'
+          id='dropdownMenuButton'
+          data-toggle='dropdown'
+          aria-expanded='false'
+        >
+          iyed
+        </button>
+      </React.Fragment>
+    )
+  }
+
   return (
     <div>
-      <Dynamictable url='https://jsonplaceholder.typicode.com/comments' />
+      <Dynamictable
+        url='https://jsonplaceholder.typicode.com/comments'
+        actionColumn={<ActionColumn />}
+        canGroupBy={false}
+        canSort={true}
+        canResize={true}
+      />
     </div>
   )
 }

3c2dc8be2bdc72f3abe7e28a0d4aea30523e6605
M@HeR <maher.benbaccar@aureskonnect.com>
2022-02-17 18:00:45 +0100

    fix: modify CustomPagination in TablePagination  component

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -1,3 +1,9 @@
+import {
+  AngleDoubleSmallLeftIcon,
+  AngleDoubleSmallRightIcon,
+  AngleSmallLeftIcon,
+  AngleSmallRightIcon,
+} from '@aureskonnect/react-ui';
 import FirstPageIcon from '@mui/icons-material/FirstPage';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
@@ -8,6 +14,7 @@ import { useTheme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 import React, { PropsWithChildren, ReactElement, useCallback } from 'react';
 import { TableInstance } from 'react-table';
+
 const rowsPerPageOptions = [10, 25, 50];
 
 // avoid all of the redraws caused by the internal withStyles
@@ -20,10 +27,7 @@ const interestingPropsEqual = (prevProps: any, nextProps: any) =>
 
 // a bit of a type hack to keep OverridableComponent working as desired
 type T = typeof _MuiTablePagination;
-const MuiTablePagination: T = React.memo(
-  _MuiTablePagination,
-  interestingPropsEqual
-) as T;
+const MuiTablePagination: T = React.memo(_MuiTablePagination, interestingPropsEqual) as T;
 
 const useStyles = makeStyles({
   root: {
@@ -37,10 +41,7 @@ interface TablePaginationActionsProps {
   count: number;
   page: number;
   rowsPerPage: number;
-  onPageChange: (
-    event: React.MouseEvent<HTMLButtonElement>,
-    newPage: number
-  ) => void;
+  onPageChange: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
 }
 
 function TablePaginationActions(props: TablePaginationActionsProps) {
@@ -48,69 +49,43 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
   const theme = useTheme();
   const { count, page, rowsPerPage, onPageChange } = props;
 
-  const handleFirstPageButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     onPageChange(event, 0);
   };
 
-  const handleBackButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     onPageChange(event, page - 1);
   };
 
-  const handleNextButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     onPageChange(event, page + 1);
   };
 
-  const handleLastPageButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
   };
 
   return (
     <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-        size="large"
-      >
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label='first page' size='large'>
         {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-        size="large"
-      >
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label='previous page' size='large'>
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-        size="large"
+        aria-label='next page'
+        size='large'
       >
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-        size="large"
+        aria-label='last page'
+        size='large'
       >
         {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
@@ -125,15 +100,15 @@ export function TablePagination<T extends Record<string, unknown>>({
     state: { pageIndex, pageSize, rowCount = instance.rows.length },
     gotoPage,
     nextPage,
+    pageOptions,
+    pageCount,
     previousPage,
+    canPreviousPage,
+    canNextPage,
     setPageSize,
   } = instance;
-
   const handleChangePage = useCallback(
-    (
-      event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
-      newPage: number
-    ) => {
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, newPage: number) => {
       if (newPage === pageIndex + 1) {
         nextPage();
       } else if (newPage === pageIndex - 1) {
@@ -144,6 +119,43 @@ export function TablePagination<T extends Record<string, unknown>>({
     },
     [gotoPage, nextPage, pageIndex, previousPage]
   );
+  function CustomPagination() {
+    return (
+      <div className='d-flex justify-content-end mr-3 pagination'>
+        <div className='d-flex align-items-center'>
+          Lignes par page :
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className='btn' onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          <AngleDoubleSmallLeftIcon height={20} width={20} />
+        </button>
+        <button className='btn' onClick={() => previousPage()} disabled={!canPreviousPage}>
+          <AngleSmallLeftIcon height={20} width={20} />
+        </button>
+        <span className='d-flex align-items-center'>
+          Page {pageIndex + 1} sur {pageOptions.length}
+        </span>
+        <button className='btn' onClick={() => nextPage()} disabled={!canNextPage}>
+          <AngleSmallRightIcon height={20} width={20} />
+        </button>
+        <button className='btn' onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          <AngleDoubleSmallRightIcon height={20} width={20} />
+        </button>
+      </div>
+    );
+  }
 
   const onChangeRowsPerPage = useCallback(
     (e: any) => {
@@ -153,19 +165,22 @@ export function TablePagination<T extends Record<string, unknown>>({
   );
 
   return rowCount ? (
-    <MuiTablePagination
-      rowsPerPageOptions={rowsPerPageOptions}
-      component="div"
-      rowsPerPage={pageSize}
-      count={rowCount}
-      page={pageIndex}
-      SelectProps={{
-        inputProps: { 'aria-label': 'rows per page' },
-        native: true,
-      }}
-      onPageChange={handleChangePage}
-      onRowsPerPageChange={onChangeRowsPerPage}
-      ActionsComponent={TablePaginationActions}
-    />
+    <React.Fragment>
+      {/* <MuiTablePagination
+        rowsPerPageOptions={rowsPerPageOptions}
+        component='div'
+        rowsPerPage={pageSize}
+        count={rowCount}
+        page={pageIndex}
+        SelectProps={{
+          inputProps: { 'aria-label': 'rows per page' },
+          native: true,
+        }}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={onChangeRowsPerPage}
+        ActionsComponent={TablePaginationActions}
+      /> */}
+      <CustomPagination />
+    </React.Fragment>
   ) : null;
 }

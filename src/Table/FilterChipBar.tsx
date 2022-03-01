d6cf2f4af8e98eea4ceef4c0f780d4023aff581f
M@HeR <maher.benbaccar@aureskonnect.com>
2022-03-01 15:17:35 +0100

    fix: modify css of chipbar of FilterChipBar component

--- src/Table/FilterChipBar.tsx
+++ src/Table/FilterChipBar.tsx
@@ -1,12 +1,14 @@
-import { StyledButton } from '@aureskonnect/react-ui';
-import { Chip } from '@mui/material';
-import { createStyles, makeStyles } from '@mui/styles';
-import React, { ReactElement, useCallback } from 'react';
 import { ColumnInstance, FilterValue, IdType, TableInstance } from 'react-table';
+import { CrossIcon, StyledButton } from '@aureskonnect/react-ui';
+import React, { ReactElement, useCallback } from 'react';
+import { createStyles, makeStyles } from '@mui/styles';
+
+import { Chip } from '@mui/material';
+
 const useStyles = makeStyles(
   createStyles({
     filtersActiveLabel: {
-      color: '#998',
+      color: '#2B2828',
       fontSize: '14px',
       paddingRight: 10,
     },
@@ -17,17 +19,39 @@ const useStyles = makeStyles(
     chipLabel: {
       fontWeight: 500,
       marginRight: 5,
+      color: '#2B2828 !important',
     },
     filterChip: {
+      border: '1px solid #626368 !important',
+      background: '#F6F6F6 0% 0% no-repeat padding-box !important',
       marginRight: '5px!important',
       marginBottom: '5px!important',
-      color: '#222',
     },
   })
 );
 
 type FilterChipBarProps<T extends Record<string, unknown>> = {
   instance: TableInstance<T>;
+};
+
+type ExpandableProps = { baseHeight: any; children: any };
+
+const Expandable = ({ baseHeight, children }: ExpandableProps) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const [overflow, setOverflow] = React.useState(false);
+  return (
+    <div
+      style={{ position: 'relative', overflow: 'hidden', height: expanded ? null : baseHeight }}
+      ref={(v) => v && setOverflow(v.offsetHeight < v.scrollHeight)}
+    >
+      {children}
+      {overflow && (
+        <button onClick={() => setExpanded(true)} style={{ position: 'absolute', bottom: 0 }}>
+          Afficher plus
+        </button>
+      )}
+    </div>
+  );
 };
 
 const getFilterValue = (column: ColumnInstance<any>, filterValue: FilterValue) => {
@@ -71,7 +95,7 @@ export function FilterChipBar<T extends Record<string, unknown>>({
       >
         Effacer tous
       </span>
-
+      {/* <Expandable baseHeight={30}> */}
       {filters &&
         allColumns.map((column) => {
           const filter = filters.find((f) => f.id === column.id);
@@ -80,14 +104,14 @@ export function FilterChipBar<T extends Record<string, unknown>>({
           return (
             value && (
               <Chip
-                // sx={{ marginRight: '8px!important' }}
                 className={classes.filterChip}
                 key={column.id}
+                deleteIcon={<CrossIcon height={10} width={10} fill='#2B2828' />}
                 label={
-                  <>
+                  <React.Fragment>
                     <span className={classes.chipLabel}>{column.render('Header')}: </span>
-                    {getFilterValue(column, value)}
-                  </>
+                    <span className={classes.chipLabel}>{getFilterValue(column, value)} </span>
+                  </React.Fragment>
                 }
                 onDelete={() => handleDelete(column.id)}
                 variant='outlined'
@@ -95,6 +119,7 @@ export function FilterChipBar<T extends Record<string, unknown>>({
             )
           );
         })}
+      {/* </Expandable> */}
     </div>
   ) : null;
 }

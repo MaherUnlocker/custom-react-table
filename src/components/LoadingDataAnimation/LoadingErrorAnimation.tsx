3cafdaa0dfbc4d5d00e5e60a7a387894a1640807
maher <maherunlocker@gmail.com>
2022-03-18 19:26:46 +0100

    fix: fix message error text

--- src/components/LoadingDataAnimation/LoadingErrorAnimation.tsx
+++ src/components/LoadingDataAnimation/LoadingErrorAnimation.tsx
@@ -5,7 +5,7 @@ export default function LoadingErrorAnimation(): React.ReactElement {
   return (
     <div className='d-flex align-items-center justify-content-center' style={{ padding: '20%' }}>
       <Alert variant='filled' severity='error'>
-        Error chargement de donnees
+        Erreur chargement de donnees
       </Alert>
     </div>
   );

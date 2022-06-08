93315156b32d422089080a49c33b5f0d492919d6
M@HeR <maherunlocker@gmail.com>
2022-06-08 17:18:43 +0100

    feat: add `default column filter`  component

---
+++ src/Table/DefaultColumnFilter.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import _uniqby from 'lodash.uniqby';
+import { FilterProps } from 'react-table';
+import { useTranslation } from 'react-i18next';
+
+import { StyledSelectInput } from '../components/assets/StyledSelectInput';
+import { StyledLabel } from '../components/assets/StyledLabel';
+import { findFirstColumn } from './Table';
+
+export default function DefaultColumnFilter<T extends Record<string, unknown>>({
+  columns,
+  column,
+  rows,
+  prepareRow,
+}: FilterProps<T>): React.ReactElement {
+  const { t } = useTranslation();
+  const { filterValue, setFilter, render } = column;
+  const [, setValue] = React.useState(filterValue || '');
+
+  // ensure that reset loads the new value
+  React.useEffect(() => {
+    setValue(filterValue || '');
+  }, [filterValue]);
+
+  const FilterArray: any[] = rows.map((row: any) => {
+    prepareRow(row);
+    return (
+      row.cells
+        .filter((cel: any) => {
+          const { key: cellKey } = cel.getCellProps();
+          // eslint-disable-next-line
+          return (cellKey as string).replace(/([^\_]*\_){2}/, '') === (column.id as string);
+        })
+        // eslint-disable-next-line
+        .map((cell: any) => {
+          return { label: String(cell.value), value: String(cell.value) };
+        })[0]
+    );
+  });
+
+  // his uniquby from lodash for get unique array of object
+  const unique: any = _uniqby(FilterArray, 'label'); //using lodash function to filter and get unique opjects
+
+  // this uniquby from lodash for get unique array of object
+  // FilterArray = _uniqby(FilterArray, 'label'); //using lodash function to filter and get unique opjects
+  // let unique: any = [...new Set(_without(FilterArray, undefined, null, 'null', 'undefined'))]; // FilterArray.filter((v, i, a) => a.indexOf(v) === i);
+
+  const isFirstColumn = findFirstColumn(columns) === column;
+  const [, setSelectedValueState] = React.useState<any[]>([]);
+
+  function handleSelectOnChangeEvent(selectedValue: any) {
+    if (selectedValue) {
+      setSelectedValueState(selectedValue);
+      //  add selected filter
+      setFilter(selectedValue.value);
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <StyledLabel htmlFor={column.id}>{render('Header')}</StyledLabel>
+      <StyledSelectInput
+        menuPlacement='auto'
+        menuPosition='fixed'
+        id={column.id}
+        name={column.id}
+        options={unique}
+        placeholder={unique.length > 0 ? t('Select...') : t('None')}
+        onChange={handleSelectOnChangeEvent}
+        autoFocus={isFirstColumn}
+        isClearable
+      />
+    </React.Fragment>
+  );
+}
